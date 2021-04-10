c8dc93b63b9592fec7036a23b2355f114736e5dc
Romain Demole <romaindemole@gmail.com>
2021-04-10 15:43:22 +0200

    Fichiers_joueurs

--- Page/DeuxJoueurs.js
+++ Page/DeuxJoueurs.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
         <View>
-            <Text>Page 1 Joueur</Text>
+            <Text>Page 2 Joueurs</Text>
         </View>
         );
     }
