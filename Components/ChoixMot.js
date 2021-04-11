dbd7dbe2293ff1dae53b221d395a44f96bf9b201
Romain Demole <romaindemole@gmail.com>
2021-04-11 17:33:32 +0200

    _ afficher 8 fois

--- Components/ChoixMot.js
+++ Components/ChoixMot.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Dimensions, ImageBackground } from 'react-native';
 
 class ChoixMot extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { word: '' }
+    }
+
+    _play() {
+        this.props.navigation.navigate('Jeu', { mot: this.state.word })
+    }
+
+    _setWord(text) {
+        if (!/[^a-zA-Z]/.test(text)) {
+            console.log(this.state.word.length)
+            this.setState({ word: text.toString().toUpperCase() })
+        } else {
+            console.log('erreur')
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                 <ImageBackground source={require('../assets/fond.png')} style={styles.image} blurRadius={2}>
-                <Text style={[styles.titre]}>Choix mot 2 Joueurs</Text>
-                </ImageBackground>
+                <ImageBackground source={require('../assets/fond_maison.png')} style={styles.image} blurRadius={2}>
+                <Text>Choisissez le mot à trouver (entre 5 et 14 lettres)</Text>
+                <TextInput style={styles.TextInput}
+                    placeholder='Choix du mot'
+                    value={this.state.word}
+                    onChangeText={(text) => this._setWord(text)}
+                    onSubmitEditing={() => this._play()}
+                />
+                <Button
+                    title='Jouer'
+                    onPress={() => this._play()}
+                    disabled={this.state.word.length < 5 || this.state.word.length > 14}
+                /></ImageBackground>
             </View>
         );
     }
@@ -26,7 +55,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight: 'bold',
         position: 'absolute',
-        top: 40,
+        top: 70,
         left: 0,
         color: 'white',
       },
--- Components/Jeu.js
+++ Components/Jeu.js
@@ -8,21 +8,33 @@ class Jeu extends React.Component {
         console.log("Menu")
     this.props.navigation.navigate('Menu')
       }
-
       constructor(props) {
         super(props);
         this.state = {
          value: this.props.navigation.state.params.mot 
          };
-       }
 
+         let nb = this.state.value.length
+
+         let mot = this.props.navigation.state.params.mot
+    
+         
+       }
     render() {
+        var myloop = [];
+
+for (let i = 0; i < 8; i++) {
+  myloop.push(
+    <Text style={[styles.titre]}> _ </Text>
+  );
+}
         console.log(this.props.navigation.state.params.mot)
-        let mot  = this.props.navigation.state.params.mot
+        let mot = this.props.navigation.state.params.mot
         return (
             <View style={styles.container}>
                 <ImageBackground source={require('../assets/fond_maison.png')} style={styles.image} blurRadius={2}>
-                <Text style={[styles.titre]}>Le mot à trouver est { mot } {"\n\n"} {this.state.value.length} lettres</Text>
+                <Text style={[styles.titre]}>Le mot à trouver est {"\n\n"} {this.state.value.length} lettres {'\n\n\n\n'}{myloop}</Text>
+                
                 </ImageBackground>
             </View>
             );
@@ -43,7 +55,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         position: 'absolute',
         top: 70,
-        left: 0,
+        left: 40,
         color: 'white',
       },
     image: {
