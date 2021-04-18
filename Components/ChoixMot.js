65baea1d18acd0ca19fd0103680c3b9a8aa91a5d
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2021-04-18 12:35:28 +0200

    Choix du mot fonctionnel

--- Components/ChoixMot.js
+++ Components/ChoixMot.js
@@ -4,41 +4,38 @@ import { StyleSheet, Text, View, Button, TextInput, Dimensions, ImageBackground 
 class ChoixMot extends React.Component {
 
     constructor(props) {
-        super(props)
-        this.state = { word: '' }
+      super(props)
+      this.state = { word: '' }
     }
 
     _play() {
-        this.props.navigation.navigate('Jeu', { mot: this.state.word })
+      this.props.navigation.navigate('Jeu', { mot: this.state.word.toUpperCase() })
     }
 
     _setWord(text) {
-        if (!/[^a-zA-Z]/.test(text)) {
-            console.log(this.state.word.length)
-            this.setState({ word: text.toString().toUpperCase() })
-        } else {
-            console.log('erreur')
-        }
+      if (!/[^a-zA-Z]/.test(text)) {
+        this.setState({ word: text })
+      }
     }
 
     render() {
-        return (
-            <View style={styles.container}>
-                <ImageBackground source={require('../assets/fond_maison.png')} style={styles.image} blurRadius={2}>
-                <Text>Choisissez le mot à trouver (entre 5 et 14 lettres)</Text>
-                <TextInput style={styles.TextInput}
-                    placeholder='Choix du mot'
-                    value={this.state.word}
-                    onChangeText={(text) => this._setWord(text)}
-                    onSubmitEditing={() => this._play()}
-                />
-                <Button
-                    title='Jouer'
-                    onPress={() => this._play()}
-                    disabled={this.state.word.length < 5 || this.state.word.length > 14}
-                /></ImageBackground>
-            </View>
-        );
+      return (
+        <View style={styles.container}>
+          <ImageBackground source={require('../assets/fond_maison.png')} style={styles.image} blurRadius={2}>
+          <Text>Choisissez le mot à trouver (entre 5 et 14 lettres)</Text>
+          <TextInput style={styles.TextInput}
+            placeholder='Choix du mot'
+            value={this.state.word}
+            onChangeText={(text) => this._setWord(text)}
+            onSubmitEditing={() => this._play()}
+          />
+          <Button
+            title='Jouer'
+            onPress={() => this._play()}
+            disabled={this.state.word.length < 5 || this.state.word.length > 14}
+          /></ImageBackground>
+        </View>
+      );
     }
 }
 
